2a9a463d813fa5393ecb84afb326be94914ffd0d
Shapokhan <shafiullah3713@gmail.com>
2022-10-20 18:21:15 +0500

    Code changes

--- src/CommentPin.js
+++ src/CommentPin.js
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
 
-
 export default function CommentPin(props) {
 	const [fillColor, setFillColor] = useState('#F38553');
 	
 	return(
 		<g>
-			<circle cx={props.offsetLeft} cy={props.offsetTop} r="12" fill={fillColor}/>
+			<circle cx={props.offsetLeft} cy={props.offsetTop} r="12" fill={fillColor} />
 			<text x={props.offsetLeft} y={props.offsetTop + 5} text-anchor="middle" fill="white" font-size="15">{props.number.toString().padStart(2, 0)}</text>
 		</g>
 		);
