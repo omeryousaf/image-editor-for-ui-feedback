47dcc52981568a1134480f23bebee9a07782c546
Shapokhan <shafiullah3713@gmail.com>
2022-11-29 20:38:42 +0500

    Dragging issue fixed

--- src/CommentTextArea.js
+++ src/CommentTextArea.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { makeStyles } from "@material-ui/core/styles";
 
@@ -8,13 +8,23 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const CommentTextArea = (props) => {
+  const [calX, setCalX] = useState(null)
+  const [calY, setCalY] = useState(null)
   const classes = useStyles();
   const onClick = (event) => {
     event.stopPropagation();
   };
+  const handleDragStart = (event) => {
+    setCalX((event.clientX + props.imgScrollRef.current.scrollLeft -
+      props.imgParentRef.current.offsetLeft) - props.offsetLeft)
+
+    setCalY((event.clientY +
+      props.imgScrollRef.current.scrollTop -
+      props.imgParentRef.current.offsetTop) - props.offsetTop)
+  }
   const handleDragEnd = (event) => {
     event.preventDefault();
-    props.dragEnd(props.id, event);
+    props.dragEnd(props.id, event, calX, calY);
   };
   return (
     <foreignObject
@@ -26,6 +36,7 @@ const CommentTextArea = (props) => {
         onClick={onClick}
         draggable="true"
         onDragEnd={handleDragEnd}
+        onDragStart={handleDragStart}
         className={classes.textarea}
       />
     </foreignObject>
