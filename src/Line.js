22984b1be9d5ba59dc5b08d7f309da4c3b0c6b37
Shapokhan <shafiullah3713@gmail.com>
2023-01-11 13:10:52 +0500

    Comments added for more redeability

--- src/Line.js
+++ src/Line.js
@@ -17,36 +17,43 @@ const CommentLine = (props) => {
     };
 
     const handleDragStart = (event) => {
-        
+        // diffInInitialLeft is the distance between the draggingX point and startX (Initial X-axis)
         diffInInitialLeft = (event.clientX + props.imgScrollRef.current.scrollLeft -
             props.imgParentRef.current.offsetLeft) - props.initialLeftOffset;
 
+        //diffInInitialTop is the distance between the draggingY point and startY (Initial Y-axis)
         diffInInitialTop = (event.clientY + props.imgScrollRef.current.scrollTop -
             props.imgParentRef.current.offsetTop) - props.initialTopOffset;
 
+        // diffInFinalLeft is the distance between the dragging-end-X point and endX (Final X-axis).
         diffInFinalLeft = props.finalLeftOffset - (event.clientX + props.imgScrollRef.current.scrollLeft - props.imgParentRef.current.offsetLeft);
 
+        // diffInFinalTop is the distance between the dragging-end-Y point and endY (Final Y-axis).
         diffInFinalTop = props.finalTopOffset - (event.clientY +
             props.imgScrollRef.current.scrollTop - props.imgParentRef.current.offsetTop);
     }
     const handleDragEnd = (event) => {
         event.preventDefault();
 
+        // Updated Point of startX (Initial-X)
         const updatedInitialLeft =
             (event.clientX +
                 props.imgScrollRef.current.scrollLeft -
                 props.imgParentRef.current.offsetLeft) - diffInInitialLeft;
 
+        // Updated Point of startY (Initial-Y)
         const updatedInitialTop =
             (event.clientY +
                 props.imgScrollRef.current.scrollTop -
                 props.imgParentRef.current.offsetTop) - diffInInitialTop;
 
+        // Updated Point of EndX (Final-X)
         const updatedFinalLeft =
             (event.clientX +
                 props.imgScrollRef.current.scrollLeft -
                 props.imgParentRef.current.offsetLeft) + diffInFinalLeft;
 
+        // Updated Point of EndY (Final-Y)
         const updatedFinalTop =
             (event.clientY +
                 props.imgScrollRef.current.scrollTop -
